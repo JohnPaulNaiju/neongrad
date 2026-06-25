f6c73a8f57023e404328c99fa748ae79ef48de0d
JohnPaulNaiju <johnpaul100574@gmail.com>
2026-06-25 18:45:15 +0530

    Added test cases for rule of 5

--- tests/test_tensor.cpp
+++ tests/test_tensor.cpp
@@ -56,5 +56,59 @@ int main() {
 
         std::cout << "Test 5 passed ✅ [fill zeros] \n";
     }
+
+    {
+        Tensor tensor1({2, 2});
+        tensor1[0] = 1.0f;
+        tensor1[1] = 2.0f;
+        tensor1[2] = 3.0f;
+        tensor1[3] = 4.0f;
+
+        Tensor tensor2 = tensor1;
+        assert(tensor1.size() == tensor2.size());
+        assert(tensor2[0] == tensor1[0]);
+
+        tensor2[0] = 99.0f;
+        assert(tensor1[0] == 1.0f && "FAIL: Shallow copy!");
+
+        std::cout << "Test 6 passed ✅ [copy constructor] \n";
+    }
+
+    {
+        Tensor tensor1({2, 2});
+        tensor1[0] = 1.0f;
+
+        Tensor tensor2({10, 10});
+        tensor2 = tensor1;
+
+        assert(tensor1.size() == tensor2.size());
+        assert(tensor2[0] == 1.0f);
+
+        std::cout << "Test 7 passed ✅ [copy assignment] \n";
+    }
+
+    {
+        Tensor tensor1({2, 2});
+        tensor1[0] = 1.0f;
+        Tensor tensor2 = std::move(tensor1);
+
+        assert(tensor2.size() == 4);
+        assert(tensor2[0] == 1.0f);
+
+        std::cout << "Test 7 passed ✅ [move constructor] \n";
+    }
+
+    {
+        Tensor tensor1({2, 2});
+        tensor1[0] = 1.0f;
+        Tensor tensor2({3, 3});
+
+        tensor2 = std::move(tensor1);
+
+        assert(tensor2.size() == 4);
+        assert(tensor2[0] == 1.0f);
+
+        std::cout << "Test 8 passed ✅ [move assignment] \n";
+    }
     return 0;
 }
