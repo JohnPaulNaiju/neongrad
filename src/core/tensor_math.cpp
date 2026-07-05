8f04ee44ce3f369a6181bdaea198990aa9b34339
JohnPaulNaiju <johnpaul100574@gmail.com>
2026-07-05 21:10:10 +0530

    Added naive GEMM for comparison with SIMD GEMM

--- src/core/tensor_math.cpp
+++ src/core/tensor_math.cpp
@@ -73,3 +73,30 @@ Tensor gemm(const Tensor& A, const Tensor& B) {
 
     return C;
 }
+
+Tensor gemm_naive(const Tensor& A, const Tensor& B) {
+
+    const std::size_t m1 = A.shape().rbegin()[1];
+    const std::size_t n1 = A.shape().back();
+    const std::size_t m2 = B.shape().rbegin()[1];
+    const std::size_t n2 = B.shape().back();
+
+    assert(n1 == m2 && "Inner dimensions of matrix do not match");
+
+    Tensor C({m1, n2});
+
+    C.fill_zeros();
+
+    for (std::size_t i = 0; i < M; ++i) {
+        for (std::size_t k = 0; k < K; ++k) {
+
+            const float a_ik = A(i, k);
+
+            for (std::size_t j = 0; j < N; ++j) {
+                C(i, j) += a_ik * B(k, j);
+            }
+        }
+    }
+
+    return C;
+}
